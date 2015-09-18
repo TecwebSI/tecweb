154cbc101ee8ff0d51a60fc311d650516f0f579f
TecwebSI <grasi645@hotmail.com>
2015-09-18 18:34:03 -0300

    1

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+	$title = "Início";
+	include "header.php";
+	include "login.php";
+
+?>
+  
+  
+  <div id="maincol">
+  
+    <div class="rule">
+      <h1>Inicio</h1>
+    </div>
+	
+    <p><strong>tralalala</strong> é um site pra te auxiliar no planejamento de consumo de energia.</p>
+	<br /><br /><br /><br />
+
+  </div>
+
+<?php include "footer.php"; ?>
