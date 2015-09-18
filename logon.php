ae412dee0a46ca3c3ed474ed1fcabc49e8c55f9d
TecwebSI <grasi645@hotmail.com>
2015-09-18 18:36:17 -0300

    2

--- logon.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	session_start();
-	$s = isSet($_POST["login"]) && $_POST["login"] != "" ? $_POST["login"] : "BLANK";
-	$_SESSION["login"] = $_SESSION["nome"] = $s;
-	header("Location: index.php");
-?>
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	session_start();
-	$_SESSION["login"] = $_SESSION["nome"] = NULL;
-	session_destroy();
-	header("Location: index.php");
-?>
